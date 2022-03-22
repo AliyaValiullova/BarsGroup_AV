51e3fdbadd2ffa5453377d8b7ba79339a54526f5
gatin <gatin@LAPTOP-GECSPVNA>
2022-03-22 14:47:01 +0300

    Добавьте файлы проекта.

---
+++ BarsGroup_AV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BarsGroup_AV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
